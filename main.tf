0bfef9e98d1832002cf437fe0b838f7590315803
Andy Badera <andybadera@microsoft.com>
2024-11-11 14:12:48 -0600

    Declaring additional vars.

--- main.tf
+++ main.tf
@@ -7,6 +7,18 @@ terraform {
   }
 }
 
+variable "ARM_CLIENT_SECRET" {
+  type = string
+}
+
+variable "ARM_CLIENT_ID" {
+  type = string
+}
+
+variable "ARM_TENANT_ID" {
+  type = string
+}
+
 variable "ARM_SUBSCRIPTION_ID" {
   type = string
 }
