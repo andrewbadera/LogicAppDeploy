fa28ad60fa08db7777c6dfc009b155e0f267a4aa
Andy Badera <andybadera@microsoft.com>
2024-11-12 12:03:53 -0600

    More workflow troubleshooting.

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ terraform {
     container_name       = "tfstate"
     key                  = "tf-example" # can be anything
     #use_oidc             = true # To use OIDC to authenticate to the backend
-    client_id            = "${var.ARM_CLIENT_ID}" # The client ID of the Managed Identity
-    subscription_id      = "${var.ARM_SUBSCRIPTION_ID}" # The subscription ID where the storage account exists
+    client_id            = "9af55caa-2954-4505-b792-8b94027b1e39" # The client ID of the Managed Identity
+    subscription_id      = "bc3ba08c-ec7c-49c9-b917-29f77a23a41b" # The subscription ID where the storage account exists
     tenant_id            = "{var.ARM_TENANT_ID}" # The tenant ID where the subscription and the Managed Identity are
   }
 }
