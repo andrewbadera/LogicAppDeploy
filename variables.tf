5400050d44511677679d8070b435be30efaab1fd
Andy Badera <andybadera@microsoft.com>
2024-11-13 12:30:29 -0600

    Chasing vars in plan.

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "ARM_CLIENT_ID" {
+  type = string
+}
+
+variable "ARM_TENANT_ID" {
+  type = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type = string
+}
+
+variable "COSMOS_DB_ACCOUNT" {
+  type = string
+}
+
+variable "COSMOS_DB_DATABASE" {
+  type = string
+}
+
+variable "COSMOS_DB_CONTAINER" {
+  type = string
+}
+
+variable "RG_NAME" {
+  type = string
+}
+
+variable "RG_LOCATION" {
+  type = string
+}
+
+variable "LOCATION_ABBREVIATION" {
+  type = string
+}
+
+variable "ENVIRONMENT" {
+  type = string
+}
