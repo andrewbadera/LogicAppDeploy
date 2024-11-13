859c06c3b8938d04716fe526eb556fd9652a56fb
Andy Badera <andybadera@microsoft.com>
2024-11-13 07:50:39 -0600

    Debugging auth.

--- main.tf
+++ main.tf
@@ -55,8 +55,8 @@ terraform {
     storage_account_name = "stterraformstateeus"
     container_name       = "tfstate"
     key                  = "tf-example" # can be anything
-    use_oidc             = true # To use OIDC to authenticate to the backend
-    #use_azuread_auth     = true
+    #use_oidc             = true # To use OIDC to authenticate to the backend
+    use_azuread_auth     = true
     #use_msi              = true
     client_id            = "9af55caa-2954-4505-b792-8b94027b1e39" # The client ID of the Managed Identity
     subscription_id      = "bc3ba08c-ec7c-49c9-b917-29f77a23a41b" # The subscription ID where the storage account exists
@@ -67,9 +67,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-  #use_azuread_auth = true
+  use_azuread_auth = true
   #use_msi          = true
-  use_oidc         = true # Use OIDC to authenticate to Azure
+  #use_oidc         = true # Use OIDC to authenticate to Azure
   subscription_id  = "${var.ARM_SUBSCRIPTION_ID}"
 }
 
