671c60412a7679f28f480a43a110be3bd0c418ef
Andy Badera <andybadera@microsoft.com>
2024-11-21 16:49:07 -0600

    logic_app_id

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "azurerm_logic_app_action_custom" "logic_app_cosmosdb_createorupdatedoc
     runAfter = {}
     type     = "ApiConnection"
   })
-  logic_app_id = format("%s/%s", "/subscriptions/${var.ARM_SUBSCRIPTION_ID}/providers/Microsoft.Logic/locations/${var.RG_LOCATION}/providers/Microsoft.Logic/workflows/", azurerm_logic_app_workflow.logic_app_workflow.name)
+  logic_app_id = format("%s/%s", "/subscriptions/${var.ARM_SUBSCRIPTION_ID}/resourceGroups/${var.RG_NAME}/providers/Microsoft.Logic/workflows/", azurerm_logic_app_workflow.logic_app_workflow.name)
   name         = "Create_or_update_document_(V3)"
   depends_on = [
     azurerm_logic_app_workflow.logic_app_workflow,
@@ -71,7 +71,7 @@ resource "azurerm_logic_app_action_custom" "logic_app_cosmosdb_createorupdatedoc
 }
 
 resource "azurerm_logic_app_trigger_http_request" "logic_app_trigger_http_request" {
-  logic_app_id = format("%s/%s", "/subscriptions/${var.ARM_SUBSCRIPTION_ID}/providers/Microsoft.Logic/locations/${var.RG_LOCATION}/providers/Microsoft.Logic/workflows/", azurerm_logic_app_workflow.logic_app_workflow.name)
+  logic_app_id = format("%s/%s", "/subscriptions/${var.ARM_SUBSCRIPTION_ID}/resourceGroups/${var.RG_NAME}/providers/Microsoft.Logic/workflows/", azurerm_logic_app_workflow.logic_app_workflow.name)
   method       = "POST"
   name         = "When_a_HTTP_request_is_received"
   schema = jsonencode({
